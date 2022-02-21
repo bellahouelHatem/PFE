4b015722e948f0debdda6fa8e9418aa25dd60354
hatem-bellahouel <80073529+hatem-bellahouel@users.noreply.github.com>
2022-02-21 22:59:27 +0100

    Hatem

---
+++ front-end/src/Compoments/Formulaire.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+export default class Formulaire  extends Component {
+    
+    render() { 
+        return (<div>
+            <h3>Formulaire</h3>
+        </div>);
+    }
+}
