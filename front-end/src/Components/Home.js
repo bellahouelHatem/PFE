49e0d949455eb227645ce7d2938354f103d2892b
hatem-bellahouel <80073529+hatem-bellahouel@users.noreply.github.com>
2022-03-28 14:46:44 +0100

    Hatem

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -1,30 +1,67 @@
-import React,{createRef, useEffect}  from "react";
-import $ from "jquery";
-import '../App.css';
-import { useLocation } from "react-router-dom";
-window.jQuery = $;
-window.$ = $;
-require("jquery-ui-sortable");
-require('formBuilder/dist/form-render.min.js');
- 
-function Home() {
-  const location= useLocation();
-  const {data}=location.state;
-    
-    var formRenderOptions = {
-        formData: JSON.parse(data),
-    dataType:'json' }
-    const fb = createRef();
-    useEffect(()=>{const formBuilder = $(fb.current).formRender(formRenderOptions);
-})
-    return (
-      <div  ref={fb}>
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../App.css";
 
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 6, 0),
+        end: new Date(2021, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 6, 7),
+        end: new Date(2021, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2021, 6, 20),
+        end: new Date(2021, 6, 23),
+    },
+];
+
+function Home() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+        <div className="App">
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
         </div>
-        
-        
-      
     );
-  }
+}
   export default Home;
 
---
+++ front-end/src/Components/PlanInspection/planing.js
@@ -0,0 +1,30 @@
+import React,{createRef, useEffect}  from "react";
+import $ from "jquery";
+import '../App.css';
+import { useLocation } from "react-router-dom";
+window.jQuery = $;
+window.$ = $;
+require("jquery-ui-sortable");
+require('formBuilder/dist/form-render.min.js');
+ 
+function Home() {
+  const location= useLocation();
+  const {data}=location.state;
+    
+    var formRenderOptions = {
+        formData: JSON.parse(data),
+    dataType:'json' }
+    const fb = createRef();
+    useEffect(()=>{const formBuilder = $(fb.current).formRender(formRenderOptions);
+})
+    return (
+      <div  ref={fb}>
+
+        </div>
+        
+        
+      
+    );
+  }
+  export default Home;
+
